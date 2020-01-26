946d3c27e8a74793cee9879e13a5d73e6f7e15d5
jacob Bruxer <46026810+bruxerjk@users.noreply.github.com>
2020-01-26 11:08:11 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Precip colors and bounds (ticks)
+
+@author: jacobb
+"""
+
+# Colors options
+
+# NWS colors
+nws_precip_colors = [
+                    "#04e9e7",  # 0.01 - 0.10 inches
+                    "#019ff4",  # 0.10 - 0.25 inches
+                    "#0300f4",  # 0.25 - 0.50 inches
+                    "#02fd02",  # 0.50 - 0.75 inches
+                    "#01c501",  # 0.75 - 1.00 inches
+                    "#008e00",  # 1.00 - 1.50 inches
+                    "#fdf802",  # 1.50 - 2.00 inches
+                    "#e5bc00",  # 2.00 - 2.50 inches
+                    "#fd9500",  # 2.50 - 3.00 inches
+                    "#fd0000",  # 3.00 - 4.00 inches
+                    "#d40000",  # 4.00 - 5.00 inches
+                    "#bc0000",  # 5.00 - 6.00 inches
+                    "#f800fd",  # 6.00 - 8.00 inches
+                    "#9854c6",  # 8.00 - 10.00 inches
+                    "black"     # 10.00+
+                    #"#fdfdfd"   # 10.00+
+                    ]
+
+# set bounds at specific precip totals for plotting with colors
+nws_bounds = [0.1, 0.5, 1.0, 2.5, 5.0,
+          7.5, 10, 15, 20, 25,
+          30, 40, 50, 75, 100,
+          150]
+
+nws_colors = {"colors" : nws_precip_colors, 
+              "bounds" : nws_bounds}
+
+
+# Canadian Precipitation Analysis colors
+capa_precip_colors = ['white', 
+                    "#98cbfe",  # 0.1
+                    "#0098fe",  # 0.5
+                    "#002dfe",  # 1.0
+                    "#00fe65",  # 2.5
+                    "#00cb00",  # 5.0
+                    "#009800",  # 7.5
+                    "#006500",  # 10.
+                    "#fefe32",  # 15.
+                    "#fecb00",  # 20.
+                    "#fe9800",  # 25
+                    "#fe6500",  # 30
+                    "#fe0000",  # 40
+                    "#fe0098",  # 50
+                    "#9832cb",  # 75
+                    '#650098',  #100
+                    "#989898"    # 150
+                    #"#fdfdfd"  # 150
+                    ]
+
+capa_bounds = [0.0, 0.1, 0.5, 1.0, 2.5, 5.0,
+          7.5, 10, 15, 20, 25,
+          30, 40, 50, 75, 100,
+          150, 250]
+
+capa_colors = {"colors" : capa_precip_colors, 
+               "bounds" : capa_bounds}
+
+
+
+
+def get_colors(colors):
+    if colors == 'capa':
+        colors = capa_colors
+    elif colors == 'nws':
+        colors = nws_colors
+    return colors
+
+
+def get_map_extent(map_extent):
+
+    # extent of plot for Great Lakes
+    if map_extent == 'glslr':
+        # entire Great Lakes St. Lawrence
+        bbox = {'xmin': -93.5, 'xmax': -69, 'ymin': 40, 'ymax': 51}
+    elif map_extent == 'erislr':
+        # Erie through St. Lawrence
+        bbox = {'xmin': -86, 'xmax': -69, 'ymin': 40, 'ymax': 51}
+    elif map_extent == 'ontslr':
+        # Ontario through St. Lawrence
+        bbox = {'xmin': -82, 'xmax': -69, 'ymin': 40, 'ymax': 51} 
+        
+    return bbox
+
+
+def get_cities(map_extent):
+    
+    # depends on map extent 
+    if map_extent == 'glslr':
+        cities = ['Duluth', 'Thunder Bay', 'Sault Ste. Marie', 
+                  'Milwaukee', 'Chicago', 'Detroit', 'Cleveland', 'Buffalo', 
+                  'Toronto', 'Rochester', 'Kingston', 
+                  'Montreal', 'Ottawa']
+    elif map_extent == 'erislr':
+        cities = ['Sault Ste. Marie',
+                  'Detroit', 'Cleveland', 'Buffalo', 
+                  'Toronto', 'Kingston', 
+                  'Montreal', 'Ottawa']
+    elif map_extent == 'ontslr':
+        cities = ['Toronto', 'Buffalo', 'Rochester', 'Kingston', 
+                  'Montreal', 'Ottawa']
+        
+    return cities
+    
---
+++ plot_merged_prcp.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Fetches and plots bi-national Canada/US "Merged" precipitation product 
+available through Midwest Regional Climate Center
+https://mrcc.illinois.edu/gismaps/naprecip.htm
+
+The “Merged” dataset is created using both the 
+Canadian Precipitation Analysis (CaPA) and the 
+Multi-sensor Precipitation Estimate (MPE) datasets.
+
+More information: 
+    https://mrcc.illinois.edu/cliwatch/northAmerPcpn/aboutArchive.html#merged
+
+@author: Jacob Bruxer
+"""
+import sys
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import matplotlib.patheffects as pe
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from scipy.interpolate import griddata
+from datetime import datetime, timedelta
+from pathlib import Path
+from config import get_colors, get_map_extent, get_cities
+
+# Parameters to modify to change map extent and dates
+
+# extent = 'glslr'  # entire Great Lakes - St. Lawrence basin
+extent = 'erislr'  # Lake Erie through St. Lawrence
+# extent = 'ontslr'  # Lake Ontario through St. Lawrence
+
+colors = 'capa'
+#colors = 'nws'
+
+# start and end date.  If equal, then 24 hrs plotted, otherwise cumulative 
+start_date = datetime(2020,1,25)
+end_date = start_date
+# end_date = datetime(2020,1,25)
+
+
+# get additional parameters from config file
+map_extent = get_map_extent(extent)
+map_cities = get_cities(extent)
+map_colors = get_colors(colors)
+
+# compute # of days and hours
+days = (end_date - start_date).days + 1
+if days < 1:
+    sys.exit()
+    
+hrs = days*24
+
+def fetch_mrcc_csv(date, dataset='Full Contintent'):
+    
+    
+    # add check that date is within last 30 days, otherwise can't fetch
+    # in this way (need to access from archive)
+    url = '''http://mrcc.illinois.edu/cliwatch/northAmerPcpn/'''
+    
+    if dataset == 'Great Lakes':
+        url = url + 'greatLakes/mrgGL_{}.csv'.format(date.strftime('%Y%m%d'))
+
+    else:
+        # entire continental dataset
+        url = url + 'northAmer/mrg_{}.csv'.format(date.strftime('%Y%m%d'))
+
+    try:
+        print('Fetching data for {}...'.format(date.strftime('%Y%m%d')))
+        print(url)
+        # add save csv here also, so it can be recreated later on
+        df = pd.read_csv(url,
+                     na_values='None', index_col = ['lat', 'lon'])
+    except:
+        print('Problem fetching data from url: {}'.format(url))
+        print('Check dates.  Aborting')
+        sys.exit()
+        
+    return df
+
+# fetch data for first date initially, append other dates after
+df = fetch_mrcc_csv(start_date)
+
+# if multiple dates, retrieve additional dates data
+if end_date and start_date < end_date: 
+
+    next_date = start_date + timedelta(days=1)
+    while next_date <= end_date:
+        tempdf = fetch_mrcc_csv(next_date)
+        df = df + tempdf
+        next_date += timedelta(days=1)
+
+# reset index to use lat lon data for plotting    
+df.reset_index(inplace=True)
+
+# data coordinates and values
+x = df.lon
+y = df.lat
+z = df.prcp_mm
+
+
+xmin = map_extent['xmin']
+xmax = map_extent['xmax']
+ymin = map_extent['ymin']
+ymax = map_extent['ymax']
+
+interval = 0.01  # resolution of plot (smaller = higher res, 0.01 smallest)
+xi = np.arange(xmin, xmax, interval)
+yi = np.arange(ymin, ymax, interval)
+
+# create mesh grid over lats and lons
+xi,yi = np.meshgrid(xi,yi)
+
+# interpolate
+zi = griddata((x,y),z,(xi,yi),method='linear')
+
+colors = map_colors['colors']
+bounds = map_colors['bounds']
+
+# plot
+fig = plt.figure(figsize=(12,6))
+ax = fig.add_subplot(111)
+
+#  plot on x, y grid and with z contours
+p = ax.contourf(xi,yi,zi,
+                bounds, 
+                colors=colors,
+                alpha=1)
+
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.05)
+
+cbar = fig.colorbar(p, cax=cax, ticks=bounds)
+
+cbar.ax.set_yticklabels(bounds)
+cbar.set_label('Precipitation (mm)', labelpad=10, rotation=270)
+
+local_folder = Path().parent
+
+try:
+    # add basin and other GIS/shapefile features for better visualization
+    # not included in repository
+    
+    gis_folder = local_folder / 'gis'
+    
+    basins=gpd.read_file(gis_folder / '''basin_SLtotal.shp''')
+    borders=gpd.read_file(gis_folder / '''politicalborders_GLSL.shp''')
+    
+    # plot list of specific cities
+    cities=gpd.read_file(gis_folder / '''Cities_GL.shp''')
+    cities.set_index('Name', inplace=True)
+    cities = cities.loc[map_cities]
+    cities.reset_index(inplace=True)
+    
+    basins.plot(ax=ax, 
+                alpha=1, 
+                facecolor='none', 
+                edgecolor='grey')
+    
+    borders.plot(ax=ax, 
+                 facecolor='none', 
+                 edgecolor='black', 
+                 linestyles='--')
+    
+    cities.plot(ax=ax, color='black')
+    
+    # add labels to cities
+    n = cities.Name
+    
+    for i, txt in enumerate(n):
+        # adds labels
+        # ax.annotate(txt, (cities.Longitude[i], cities.Latitude[i]))
+        
+        # adds labels with white background
+        ax.text(cities.Longitude[i], cities.Latitude[i], txt,
+                  size=10,
+                  color='black',
+                  verticalalignment='bottom',
+                  path_effects=[pe.withStroke(linewidth=2, foreground="white")])
+        
+except:
+    print('''Couldn't find and/or add additional GIS data''')
+
+ax.set_xlim(xmin, xmax)
+ax.set_ylim(ymin, ymax)
+
+ax.set_title('{}-Hour Accumulated Precipitation : Valid {} 12 UTC'.format(
+                str(hrs), start_date.strftime('%Y-%m-%d')))
+
+
+out_folder = local_folder / 'graphics'
+
+# save figure
+fn = 'Precip_MrgCaPA-MPE_{}hr_Accum_{}'.format(str(hrs), 
+                                               start_date.strftime('%Y%m%d'))
+
+if start_date != end_date:
+    fn = fn + '-{}'
+    fn = fn.format(end_date.strftime('%Y%m%d'))
+    
+plt.savefig(out_folder / fn.format(), dpi=100)
+plt.close(fig)
